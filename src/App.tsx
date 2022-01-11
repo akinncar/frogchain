6425adddaf2781b2b0c51365aa1fbfafd2c2b0eb
Akinn Rosa <akinncar@hotmail.com>
2022-01-10 21:33:44 -0300

    feat: add prettier :art:

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App(): JSX.Element {
   return (
-    <View style={[StyleSheet.absoluteFill, {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center'
-    }]}>
+    <View
+      style={[
+        StyleSheet.absoluteFill,
+        {
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      ]}
+    >
       <Text>Sled</Text>
     </View>
   );
