7f5b6ea2aa46705991eaa7c35f394fd07b451ae2
akinncar <akinncar@hotmail.com>
2022-07-18 22:10:22 -0300

    fix: capitalize style

--- src/components/asset/ChartSwitcher.tsx
+++ src/components/asset/ChartSwitcher.tsx
@@ -21,7 +21,7 @@ export function ChartSwitcher({
       )}
       onPress={onPress}
     >
-      <Text style={tw`text-white captalize`}>{label}</Text>
+      <Text style={tw`text-white capitalize`}>{label}</Text>
     </TouchableOpacity>
   );
 }
