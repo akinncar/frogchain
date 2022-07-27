bf85e267415b8e8b9df8411c71aa7dcc1299d0e7
akinncar <akinncar@hotmail.com>
2022-07-27 20:23:50 -0300

    feat: add MATIC and AVAXC

--- src/components/ui/svg/asset-icon/index.ts
+++ src/components/ui/svg/asset-icon/index.ts
@@ -1,7 +1,15 @@
+import Avax from './Avax';
 import Btc from './Btc';
 import Eth from './Eth';
 import Ftm from './Ftm';
+import Matic from './Matic';
 
-const icons = { bitcoin: Btc, ethereum: Eth, fantom: Ftm };
+const icons = {
+  avalanche: Avax,
+  bitcoin: Btc,
+  ethereum: Eth,
+  fantom: Ftm,
+  matic: Matic,
+};
 
 export default icons;
--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -23,4 +23,22 @@ export const assets = {
     mainnet: 'https://rpc.ankr.com/fantom',
     apiUrl: 'https://api.ftmscan.com/api',
   },
+  matic: {
+    name: 'matic',
+    label: 'Matic',
+    ticker: 'MATIC',
+    webSocketTicker: 'matic',
+    chainId: 137,
+    mainnet: 'https://rpc.ankr.com/polygon',
+    apiUrl: 'https://api.polygonscan.com/api',
+  },
+  // avalanche: {
+  //   name: 'avalanche',
+  //   label: 'Avalanche C-Chain',
+  //   ticker: 'AVAX',
+  //   webSocketTicker: 'avax',
+  //   chainId: 43114,
+  //   mainnet: 'https://rpc.ankr.com/avalanche',
+  //   apiUrl: 'https://api.avax.network/api',
+  // },
 };
